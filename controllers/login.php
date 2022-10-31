0a95e4b2bcf5b155d5cf1475b702a021c791f7a2
Eduard <10000083.joan23@fje.edu>
2022-10-31 15:16:06 +0100

    Validacion PHP login mejorada

--- controllers/login.php
+++ controllers/login.php
@@ -1,47 +1,48 @@
 <?php
  
-    require_once '../model/conexion.php';
-
+require_once '../model/conexion.php';
+require_once './validate.php';
 
     
-    $correo = $_POST['correo'];
-    $contraseña = $_POST['contraseña'];
-    $passwordHash = sha1($contraseña);      
+$correo = $_POST['correo'];
+$contraseña = $_POST['contraseña'];
+$passwordHash = sha1($contraseña);      
  
-    $correo = $connection -> real_escape_string($correo);
+$correo = $connection -> real_escape_string($correo);
     
-    $sql = "SELECT * FROM tbl_profesores WHERE correo_profe = '$correo' and contraseña_profe = '$passwordHash'";
+$sql = "SELECT * FROM tbl_profesores WHERE correo_profe = '$correo' and contraseña_profe = '$passwordHash'";
 
-   $stmt=mysqli_stmt_init($connection);
+$stmt=mysqli_stmt_init($connection);
    
-   if(!mysqli_stmt_prepare($stmt,$sql)){
+if(!mysqli_stmt_prepare($stmt,$sql)){
        header('Location:../registro.php?error=errorconexion');
     exit();
 
 }
-   mysqli_stmt_bind_param($stmt,"s",$correo,$passwordHash);
-    mysqli_stmt_execute($stmt);
-    $resultadoconsulta=mysqli_stmt_get_result($stmt);
-    if($row=mysqli_fetch_assoc($resultadoconsulta)){
-        $row=true;
-    }else{
-        $error=false;
-    }
-    mysqli_stmt_close($stmt);
-    return $error;
-
-    // $resultados = mysqli_query($connection,$sql);
-    $num=mysqli_num_rows($resultadoconsulta);
+if(registroCamposVacios($correo,$contraseña)!==FALSE){
+    header('Location:../index.php?error=camposvacios');  
+    exit();
+}
+if(errorEmail($correo)!==FALSE){
+    header('Location:../index.php?error=erroremail');
+    exit();
+}
+if(checkusername($correo,$connection)!==FALSE){
+    header('Location:../index.php?error=checkuser');
+    exit();
+}
+$resultados = mysqli_query($connection,$sql);
+$num=mysqli_num_rows($resultados);
    
     
 
-    if ($num==1){          
-        session_start();        
-        $_SESSION['correo'] = $correo;
-        echo"<script>window.location.href = '../view/vista.php' </script>";
-    }else{               
-        session_destroy();             
-        echo"<script>window.location.href = '../index.html' </script>";
-    }
+if ($num==1){          
+    session_start();        
+    $_SESSION['correo'] = $correo;
+    echo"<script>window.location.href = '../view/vista.php' </script>";
+}else{               
+    session_destroy();             
+    echo"<script>window.location.href = '../index.html' </script>";
+}
  
 ?>
