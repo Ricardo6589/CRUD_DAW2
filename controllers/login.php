a06293aeb2b5e31210b594bb1e97c6fd1a97276b
Eduard <10000083.joan23@fje.edu>
2022-10-31 14:39:03 +0100

    Validacion PHP login

--- controllers/login.php
+++ controllers/login.php
@@ -12,9 +12,28 @@
     
     $sql = "SELECT * FROM tbl_profesores WHERE correo_profe = '$correo' and contraseña_profe = '$passwordHash'";
 
-    $resultados = mysqli_query($connection,$sql);
-    $num=mysqli_num_rows($resultados);
+   $stmt=mysqli_stmt_init($connection);
    
+   if(!mysqli_stmt_prepare($stmt,$sql)){
+       header('Location:../registro.php?error=errorconexion');
+    exit();
+
+}
+   mysqli_stmt_bind_param($stmt,"s",$correo,$passwordHash);
+    mysqli_stmt_execute($stmt);
+    $resultadoconsulta=mysqli_stmt_get_result($stmt);
+    if($row=mysqli_fetch_assoc($resultadoconsulta)){
+        $row=true;
+    }else{
+        $error=false;
+    }
+    mysqli_stmt_close($stmt);
+    return $error;
+
+    // $resultados = mysqli_query($connection,$sql);
+    $num=mysqli_num_rows($resultadoconsulta);
+   
+    
 
     if ($num==1){          
         session_start();        
