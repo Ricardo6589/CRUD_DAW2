b0d2b38e3d892dc9ee05a013f3219aaf3727ff44
AlbertFenoy <71765955+AlbertFenoy@users.noreply.github.com>
2022-11-04 18:06:29 +0100

    Add files via upload

--- view/modificarvista.php
+++ view/modificarvista.php
@@ -26,7 +26,7 @@ if (!isset($_SESSION['admin'])) {
                             <div class="btn_group">
                                 <div class="form-group align-items-center">
                                     <form action="../controllers/modificar.php" method="POST" enctype="multipart/form-data" onchange="return validaFormulario();">
-                                    <input type="submit" class="btn btn-warning" value="Modificar">
+                                        <input type="submit" class="btn btn-warning" value="Modificar">
                                     </form>
                                     <form action="./vista.php">
                                         <button class="btn btn-default" title="Salir">
@@ -53,59 +53,59 @@ if (!isset($_SESSION['admin'])) {
                                 <th>Imagen</th>
                             </tr>
                         </thead>
-                        <tbody>
-                            <tr>
-                                <div class="form-group align-items-center">
-                                    <form action="../controllers/modificar.php" method="POST" enctype="multipart/form-data" onchange="return validaFormulario();">
-
-                                        <input type="hidden" name="id" value="<?php echo $alumno['id']; ?>">
+                        <div class="form-group align-items-center">
+                            <form action="../controllers/modificar.php" method="POST" enctype="multipart/form-data" onchange="return validaFormulario();">
+                                <input type="hidden" name="id" value="<?php echo $alumno['id']; ?>">
+                                <tbody>
+                                    <tr>
                                         <td style="color:black">
-                                            <input type=" text " id="element_1" name="nombre" value="<?php echo $alumno['nombre_alu']; ?>" required /> <br><br>
+                                            <input type=" text " id="element_1" name="nombre" value="<?php echo $alumno['nombre_alu']; ?>" required size="5px" /> <br><br>
                                             <span id="element_1_msg" style="color:red"></span>
                                         </td>
                                         <td style="color:black">
 
-                                            <input type="text" id="element_2.1" name="apellido1" value="<?php echo $alumno['primer_apellido_alu']; ?>" required /> <br><br>
+                                            <input type="text" id="element_2.1" name="apellido1" value="<?php echo $alumno['primer_apellido_alu']; ?>" required size="10px" /> <br><br>
                                             <span id="element_2.1_msg" style="color:red"></span>
                                         </td>
                                         <td style="color:black">
 
-                                            <input type="text" id="element_2.2" name="apellido2" value="<?php echo $alumno['segundo_apellido_alu']; ?>" required /> <br><br>
+                                            <input type="text" id="element_2.2" name="apellido2" value="<?php echo $alumno['segundo_apellido_alu']; ?>" required size="12px" /> <br><br>
                                             <span id="element_2.2_msg" style="color:red"></span>
                                         </td>
                                         <td style="color:black">
 
-                                            <input type="text" id="element_3" name="correo" value="<?php echo $alumno['correo_alu']; ?>" required /> <br><br>
+                                            <input type="text" id="element_3" name="correo" value="<?php echo $alumno['correo_alu']; ?>" required size="5px" /> <br><br>
                                             <span id="element_3_msg" style="color:red"></span>
                                         </td>
                                         <td style="color:black">
 
-                                            <input type="text" id="element_4" name="contraseña" value="<?php echo $alumno['contraseña_alu']; ?>" required /> <br><br>
+                                            <input type="text" id="element_4" name="contraseña" value="<?php echo $alumno['contraseña_alu']; ?>" required size="8px" /> <br><br>
                                             <span id="element_4_msg" style="color:red"></span>
                                         </td>
                                         <td style="color:black">
 
-                                            <input type="text" id="element_5" name="dni" value="<?php echo $alumno['dni_alu']; ?>" required /> <br><br>
+                                            <input type="text" id="element_5" name="dni" value="<?php echo $alumno['dni_alu']; ?>" required size="8px" /> <br><br>
                                             <span id="element_5_msg" style="color:red"></span>
                                         </td>
                                         <td style="color:black">
-
-                                            <input type="text" id="element_6" name="telefono" value="<?php echo $alumno['telefono_alu']; ?>" required /> <br><br>
+                                            <input type="text" id="element_6" name="telefono" value="<?php echo $alumno['telefono_alu']; ?>" required size="8px" /> <br><br>
                                             <span id="element_6_msg" style="color:red"></span>
                                         </td>
                                         <td>
-
                                             <input type="file" id="element_7" name="img" value="<?php echo $alumno['img_alu']; ?>" required /> <br><br>
                                             <span id="element_7_msg" style="color:red"></span>
                                         </td>
 
-                                    </form>
-
-                                </div>
-                            </tr>
-                        <?php
-                        echo "</tr>";
-                    }
-                        ?>
-                        </tbody>
+                                    </tr>
+                                <?php
+                                echo "</tr>";
+                            }
+                                ?>
+                                </tbody>
+                            </form>
+                        </div>
                     </table>
+                </div>
+            </div>
+        </div>
+    </div>
